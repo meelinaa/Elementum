bf5824cda583d90758d838f15c3e1d818e32c49d
meelinaa <melinakiefer@hotmail.de>
2026-03-02 11:37:04 +0100

    feat(api): add ServiceAPI project initial setup

---
+++ Elementum-Services/Elementum-ServiceApi/Controllers/FrontendService.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Elementum.Shared.Objects;
+
+namespace Elementum_ServiceApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FrontendService : Controller
+    {
+        [HttpGet(Name = "GetMetals")]
+        public IEnumerable<Metals> Get()
+        {
+            // Logic
+            return null; 
+        }
+    }
+}
